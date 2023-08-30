622a86d0121f14aedbc3f31dced272c3f64a1da9
Firmino Massango <firminorafael0@gmail.com>
2023-08-30 16:21:44 +0200

    feat: add basic styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+import { Zen_Dots } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
+const zenDots = Zen_Dots({ weight: '400', subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`min-h-screen p-32  ${montserrat.className}`}
     >
- 	<h1>FM</h1>
+        <div className="flex justify-between">
+            <h1 className={`flex text-white text-9xl ${zenDots.className}`}>Loremip<br/> Ipsumdo</h1>
+            <span className="mt-16 uppercase text-5xl font-semibold bg-gradient-to-r from-zinc-800 to-zinc-500 bg-clip-text text-transparent">Lorem ipsum dolor</span>
+        </div>
+        <p className="text-white mt-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur laboriosam maxime officia.</p>
+
     </main>
   )
 }
