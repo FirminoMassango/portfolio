51aab0f10b68063164bad68a94cd1dffd9e88522
Firmino Massango <firminorafael0@gmail.com>
2023-08-31 17:07:55 +0200

    feat: add some responsive properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,17 @@ export default function Home() {
     <main
       className={`min-h-screen p-28 ${montserrat.className}`}
     >
-        <div className="flex justify-between">
+        <div className="lg:flex justify-between">
             <div className="">
                 <h1 className={`text-white text-9xl ${zenDots.className}`}>Loremip<br/> Ipsumdo</h1>
             </div>
             <div className="">
-                <span className="absolute top-16 right-28 mx-auto pt-32 uppercase text-5xl font-bold bg-gradient-to-r from-zinc-800 to-zinc-500 bg-clip-text text-transparent">Lorem ipsum dolor</span>
+                <span className="lg:absolute top-16 right-28 mx-auto pt-32 uppercase text-5xl font-bold bg-gradient-to-r from-zinc-800 to-zinc-500 bg-clip-text text-transparent">Lorem ipsum dolor</span>
             </div>
         </div>
         <div className="flex justify-between">
             <div>
-                <p className="w-1/2 text-white text-2xl mt-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur laboriosam maxime officia.</p>
+                <p className="w-3/5 text-white text-xl mt-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur laboriosam maxime officia.</p>
                 <button className="text-white border border-white p-3 rounded mt-16">Get in Touch --></button>
             </div>
             <div className="flex flex-col items-center justify-between text-white h-auto">
