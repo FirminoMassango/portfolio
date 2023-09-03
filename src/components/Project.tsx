eb7a12c7804a73198823e524c11163cfc9db3f5d
Firmino Massango <firminorafael0@gmail.com>
2023-09-03 10:08:20 +0200

    feat: add contact me link

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,7 +8,7 @@ interface ProjectProps {
 
 function Project(props: ProjectProps) {
   return (
-    <div className="relative w-full h-64 mb-0 border border-white rounded text-center">
+    <div className="relative w-full h-60 mb-0 border border-white rounded text-center">
       <Image src={props.projectCover} alt="Picture of the author" fill />
       {/* <span className="relative top-44 bg-zinc-800/80 py-2 px-8 rounded">
         {props.name}
