05fa784de4b701c0a0df7f5a544c4a8a95b69e84
Firmino Massango <firminorafael0@gmail.com>
2023-09-14 20:16:24 +0200

    refactor: cleaning up the code

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,40 +5,27 @@ interface TableProps {
 }
 
 function Table(props: TableProps) {
+  const colNumber = props.thirdCol ? "md:grid-cols-3" : "md:grid-cols-2";
+
   return (
     <>
-      {props.thirdCol ? (
-        <div className={`w-full grid md:grid-cols-3 text-center`}>
-          <div className="font-semibold text-sm md:text-base border-2 border-zinc-500 py-8">
-            {props.firstCol}
-          </div>
-          <div
-            className={`font-semibold text-sm md:text-base border-2 border-zinc-500 border-y-0 md:border-y-2 md:border-x-0 py-8 ${
-              !props.thirdCol ? "border-b-2 md:border-r-2" : ""
-            }`}
-          >
-            {props.secondCol}
-          </div>
-          {props.thirdCol && (
-            <div className="font-semibold text-sm md:text-base border-2 border-zinc-500 py-8">
-              {props.thirdCol}
-            </div>
-          )}
+      <div className={`w-full grid ${colNumber} text-center`}>
+        <div className="font-semibold text-sm md:text-base border-2 border-zinc-500 py-8">
+          {props.firstCol}
         </div>
-      ) : (
-        <div className={`w-full grid md:grid-cols-2 text-center`}>
-          <div className="font-semibold text-sm md:text-base border-2 border-zinc-500 py-8">
-            {props.firstCol}
-          </div>
-          <div
-            className={`font-semibold text-sm md:text-base border-2 border-zinc-500 border-y-0 md:border-y-2 md:border-x-0 py-8 ${
-              !props.thirdCol ? "border-b-2 md:border-r-2" : ""
-            }`}
-          >
-            {props.secondCol}
-          </div>
+        <div
+          className={`font-semibold text-sm md:text-base border-2 border-zinc-500 border-y-0 md:border-y-2 md:border-x-0 py-8 ${
+            !props.thirdCol ? "border-b-2 md:border-r-2" : ""
+          }`}
+        >
+          {props.secondCol}
         </div>
-      )}
+        {props.thirdCol && (
+          <div className="font-semibold text-sm md:text-base border-2 border-zinc-500 py-8">
+            {props.thirdCol}
+          </div>
+        )}
+      </div>
     </>
   );
 }
